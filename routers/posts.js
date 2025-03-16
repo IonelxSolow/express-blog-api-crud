74db6ec77c29ef034efde82792354c95af177c32
IonelxSolow <ionelpotoroaca92@gmail.com>
2025-03-16 19:23:42 +0100

    first commit

---
+++ routers/posts.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const router = express.Router();
+const postsControllers = require("../controllers/postsControllers")
+
+
+
+//index
+router.get('/', postsControllers.index);
+
+//show
+router.get('/:slug', postsControllers.show);
+
+//create
+router.post('/', postsControllers.create)
+
+//update
+router.put('/:id', postsControllers.update)
+
+//modify
+router.patch('/:id', postsControllers.modify)
+
+//destroy
+router.delete('/:id', postsControllers.destroy)
+
+module.exports = router;
---
+++ server.js
@@ -0,0 +1,22 @@
+//import express
+const express = require("express");
+const app = express();
+const port = 3000;
+
+const postsRouter = require('./routers/posts');
+
+app.use('/posts', postsRouter)
+
+// first route 
+app.get('/', (req, res) =>{
+    res.send('Hello World')
+});
+//midleware serve static files from the "public" folder
+app.use(express.static('public'));
+
+
+
+// app listening on port: 3000
+app.listen(port, () => {
+    console.log(`Server running on port http://localhost:${port}`)
+})
